ef9b607b47a190eb8a8ec3166376db98ab9584b6
キューマン・エノビクト <30486982+Qman11010101@users.noreply.github.com>
2026-03-22 03:49:14 +0900

    feat: normalizeIncludeDeleted関数のロジックを簡素化し、条件を一行で返すように変更

--- internal/usecase/helpers.go
+++ internal/usecase/helpers.go
@@ -5,13 +5,5 @@ import "github.com/chunisupport/chunisupport-api/internal/info"
 // normalizeIncludeDeleted は includeDeleted フラグを正規化します。
 // requesterAccountTypeID が EDITOR 権限を満たさない場合は常に false を返します。
 func normalizeIncludeDeleted(includeDeleted bool, requesterAccountTypeID *int) bool {
-	if !includeDeleted {
-		return false
-	}
-
-	if requesterAccountTypeID == nil || !info.HasRole(*requesterAccountTypeID, info.AccountTypeEditor) {
-		return false
-	}
-
-	return true
+	return includeDeleted && requesterAccountTypeID != nil && info.HasRole(*requesterAccountTypeID, info.AccountTypeEditor)
 }
