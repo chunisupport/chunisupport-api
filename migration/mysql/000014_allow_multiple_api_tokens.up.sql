0c5f057cbda81be6238e8b2d1edcc1daafd1c295
キューマン・エノビクト <30486982+Qman11010101@users.noreply.github.com>
2026-05-05 00:06:20 +0900

    feat: APIトークンのデフォルト名を「API Key」に変更

--- migration/mysql/000014_allow_multiple_api_tokens.up.sql
+++ migration/mysql/000014_allow_multiple_api_tokens.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE api_tokens
-    ADD COLUMN name VARCHAR(15) NOT NULL DEFAULT 'APIキー' AFTER user_id,
+    ADD COLUMN name VARCHAR(15) NOT NULL DEFAULT 'API Key' AFTER user_id,
     ADD INDEX idx_api_tokens_user_id (user_id),
     DROP INDEX uq_api_tokens_user_id;
